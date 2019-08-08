f6701f64d23977c3d08c77d2201a9d4c2bafb9d8
spartars <spartars@yandex.ru>
2019-08-08 21:50:50 +0300

    rf

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -4,9 +4,6 @@ class Comment extends Component {
     render() {
         return (
             <div className="d-flex justify-content-center">
-                <div className="spinner-border text-center" role="status">
-
-                </div>
             </div>
         );
     }
